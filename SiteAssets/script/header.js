0a7055a6e51af27b8a26796613720ccb60d98d31
João Simionato Neto <jsimionatoneto2005@gmail.com>
2023-09-27 21:25:38 -0300

    ✨ feat: finalizando projeto

---
+++ SiteAssets/script/header.js
@@ -0,0 +1,6 @@
+function showNavBar(){
+    document.getElementById("header").classList.add("change-header");
+    document.getElementById("menu").classList.add("change-nav");
+    document.getElementById("content").classList.add("change-header");
+    
+}
